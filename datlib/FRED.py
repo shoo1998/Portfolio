8f585bb70d158678605a5f2110bd01e1f9cc9795
shoo1998 <logan.r.schumacher@ndsu.edu>
2020-10-11 23:31:52 -0500

    “message”

---
+++ datlib/FRED.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  8 12:36:36 2020
+
+@author: shoo
+"""
+
+#FRED.py
+import pandas as pd
+import pandas_datareader.data as web
+import datetime
+
+def gather_data(data_codes, start, end = datetime.datetime.today(), freq = "A"):
+    i = 0
+    # dct.items() calls key and value that key points to
+    for key, val in data_codes.items():
+        if i == 0:
+            # Create dataframe for first variable, then rename column
+            df = web.DataReader(val, "fred", start, end).resample(freq).mean()
+            df.rename(columns = {val:key}, inplace = True) 
+            i = None
+        else:
+            # If dataframe already exists, add new column
+            df[key] = web.DataReader(val, "fred", start, end).resample(freq).mean()
+
+    return df
+
+def bil_to_mil(series):
+    return series.mul(10**3)
